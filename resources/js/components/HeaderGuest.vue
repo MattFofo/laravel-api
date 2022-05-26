165bf4945d23126da1f5f536d1ba78c47c7638bb
MattFofo <matteo.segattini2@gmail.com>
2022-05-26 16:36:44 +0200

    collegato vue e inserito un componente

---
+++ resources/js/components/HeaderGuest.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        sono l'header con vue
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderGuest'
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/views/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <HeaderGuest />
+    </div>
+</template>
+
+<script>
+import HeaderGuest from "../components/HeaderGuest";
+
+export default {
+    name: 'App',
+    components: {
+        HeaderGuest,
+    }
+}
+</script>
+
+<style>
+
+</style>
