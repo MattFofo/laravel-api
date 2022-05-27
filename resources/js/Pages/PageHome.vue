030fb0dfaa7ae5d471c0517c0808454ab17eb06f
MattFofo <matteo.segattini2@gmail.com>
2022-05-27 21:04:46 +0200

    fixed layout carousel

--- resources/js/Pages/PageHome.vue
+++ resources/js/Pages/PageHome.vue
@@ -16,11 +16,11 @@
                 </div>
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <button class="carousel-control carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <button class="carousel-control carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
@@ -45,6 +45,32 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+    #carouselExampleControls {
+        .carousel-control:hover  .carousel-control-next-icon, .carousel-control:hover .carousel-control-prev-icon{
+            opacity: 1;
+        }
+
+        .carousel-control-next-icon {
+            position: absolute;
+            top: 50%;
+            right: 20px;
+            filter: invert(100);
+            opacity: .5;
+            &:hover {
+                opacity: 1;
+            }
+        }
+        .carousel-control-prev-icon {
+            position: absolute;
+            top: 50%;
+            left: 20px;
+            filter: invert(100);
+            opacity: .5;
+            &:hover {
+                opacity: 1;
+            }
+        }
+    }
 
 </style>
